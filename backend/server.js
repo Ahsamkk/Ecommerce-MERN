33ddf79b5452127e54166679426baaad86ba40ef
Ahsam <ahsamkk12@gmail.com>
2025-11-08 20:22:24 +0500

    updated files

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,12 @@ import adminOrderRouter from "./routes/adminOrder.route.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://rabbit-ecommerce-xi.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
